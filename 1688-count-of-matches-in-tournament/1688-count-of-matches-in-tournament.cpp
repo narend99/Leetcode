e455c7f9bb89082d7a3189f8fcce59cca8c1b012
narend99 <122995550+narend99@users.noreply.github.com>
2023-12-05 19:45:31 +0530

    Update 1688-count-of-matches-in-tournament.cpp

--- 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int numberOfMatches(int n) 
     {
-/*-------------O(n) Aproach--------------*/
+/*-------------O(log(n)) Approach--------------*/
 
 /*      int team = n;
         int match = 0;
@@ -23,11 +23,14 @@ public:
 
         return match;
     }
+
+    //TC - O(log n)
+    //SC - O(1)
 */
 
 /*-------------O(1) Approach --------------*/
             return n-1;     //Two Ways 
-                            //1. Finding Pattern by running different Test Cases
-                            //2. if n is the total team, that means 1 will be the winner and n-1 will be eliminated
+    //TC- O(1)              //1. Finding Pattern by running different Test Cases
+    //SC- O(1)              //2. if n is the total team, that means 1 will be the winner and n-1 will be eliminated
     }                       // in a single match played b/w two team one gets eleminated 
 };                          //so that means for n-1 eliminations n-1 matches needs to be played  
